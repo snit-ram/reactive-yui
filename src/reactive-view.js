f9314fa2be58b8d1e6318b461d4a4b9ecab4adce
Julio Nobrega Netto <inerte@gmail.com>
2013-11-26 18:52:47 +0000

    Removed beforeRender check
    It's no longer needed. When reactive-yui was calling render()
    multiple times, it was necessary to limit it, but that's no
    longer true

--- src/reactive-view.js
+++ src/reactive-view.js
@@ -2,11 +2,9 @@ YUI.add("reactive-view", function (Y) {
     "use strict";
 
     Y.ReactiveView = Y.Base.create("ReactiveView", Y.View, [Y.Reactive], {
-        beforeRender: function () {},
         afterRender: function () {},
 
-        destructor: function () {
-        },
+        destructor: function () {},
 
         render: function () {
             var self = this;
@@ -18,10 +16,6 @@ YUI.add("reactive-view", function (Y) {
             }
 
             var contents = self.template(self);
-
-            if (self.beforeRender() === false) {
-                return this; //abort rendering
-            }
 
             self.get('container').setContent(contents);
             self.afterRender();
