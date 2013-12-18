1e2ac63b974e7d4c39fdc57c46dc7ddee1eecb1d
Rafael Martins <snit.ram@gmail.com>
2013-12-18 00:35:17 -0800

    getById and getByClientId should notify only the added/removed ids

--- src/reactive-model-list.js
+++ src/reactive-model-list.js
@@ -1,17 +1,52 @@
 YUI.add("reactive-model-list", function (Y) {
     "use strict";
 
+    function arrayDiff(a, b) {
+        return a.filter(function (item) {
+            return b.indexOf(item) === -1;
+        }).concat(b.filter(function (item) {
+            return a.indexOf(item) === -1;
+        }));
+    }
+
     Y.ReactiveModelList = Y.Base.create("ReactiveModelList", Y.ModelList, [Y.Reactive], {
         initializer: function () {
             this._reactivePendingChanges = [];
-            Y.Do.before(this._trackList, this, "getById", this);
-            Y.Do.before(this._trackList, this, "getByClientId", this);
+            this._idDeps = {};
+            this._clientIdDeps = {};
             Y.Do.before(this._trackList, this, "item", this);
             Y.Do.before(this._trackList, this, "each", this);
             Y.Do.before(this._trackList, this, "size", this);
             Y.Do.before(this._trackList, this, "some", this);
             Y.Do.before(this._trackList, this, "filter", this);
             Y.Do.before(this._trackList, this, "toArray", this);
+
+            this._trackGetById('id');
+            this._trackGetById('clientId');
+        },
+
+        _trackGetById: function (idPropertyName) {
+            var depsPropertyName = '_' + idPropertyName + 'Deps';
+
+            this.after(['add', 'remove'], function (e) {
+                var id = e.model.get(idPropertyName);
+                if (this[depsPropertyName][id]) {
+                    this[depsPropertyName][id].changed();
+                }
+            }, this);
+
+            this.on('reset', function (e) {
+                var oldIds = this.get(idPropertyName),
+                    newIds = Y.Array.map(e.models, function (model) {
+                        return model.get(idPropertyName);
+                    }, this);
+
+                arrayDiff(oldIds, newIds).forEach(function (id) {
+                    if (this[depsPropertyName][id]) {
+                        this[depsPropertyName][id].changed();
+                    }
+                }, this);
+            }, this);
         },
 
         _trackList: function () {
@@ -26,6 +61,18 @@ YUI.add("reactive-model-list", function (Y) {
             }
 
             this._deps._YUIModelListDependency.depend();
+        },
+
+        getById: function (id) {
+            this._idDeps[id] = this._idDeps[id] || new Y.Deps.Dependency();
+            this._idDeps[id].depend();
+            return Y.ModelList.prototype.getById.call(this, id);
+        },
+
+        getByClientId: function (id) {
+            this._clientIdDeps[id] = this._clientIdDeps[id] || new Y.Deps.Dependency();
+            this._clientIdDeps[id].depend();
+            return Y.ModelList.prototype.getByClientId.call(this, id);
         }
     });
 }, "@VERSION@", {
